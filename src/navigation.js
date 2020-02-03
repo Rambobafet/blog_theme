ebdd5a194c0be8de53c9833ddb9c9e3c85454a9f
Sébastien Picq <sebastien.picq@ekino.com>
2020-02-04 00:10:19 +0100

    feat(theme): continue working on the static integration

--- src/navigation.js
+++ src/navigation.js
@@ -1,7 +1,19 @@
 const toggleNav = document.getElementById('toggleNavigation');
+const panelNav = document.getElementById('mainNavigationPanel');
+
+function stringToBoolean(value) {
+    return value === 'true'
+}
 
 toggleNav.addEventListener('click', (e) => {
     console.log('e', e)
 
-    toggleNav.setAttribute('aria-expanded', toggleNav.getAttribute('aria-expanded') === 'true' ? 'false' : 'true')
+    const expanded = stringToBoolean(toggleNav.getAttribute('aria-expanded'));
+
+    toggleNav.setAttribute('aria-expanded', (!expanded).toString());
+    panelNav.setAttribute('aria-hidden', (!expanded).toString());
+
+    expanded
+        ? panelNav.classList.remove('is-open')
+        : panelNav.classList.add('is-open')
 })
